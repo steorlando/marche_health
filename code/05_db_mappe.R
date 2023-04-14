2df71afde24a2c57b2e9bcbbcace38cdadf7a671
Mariagrazia Cicala <mariagraziacicala@AirdiMaiagrazia.lan>
2023-04-14 12:32:42 +0200

    Percentuale over 65 e over 80 su tot_over. Indice invecchiamento

--- code/05_db_mappe.R
+++ code/05_db_mappe.R
@@ -24,6 +24,26 @@ comuni_map <- comuni_map %>%
   mutate(perc_lim = pop_lim/popolazione,
          perc_lim2 = pop_lim2/popolazione)
 
+#Percentuale over (sia 65 che 80) su tot over 
+comuni_map$tot_pop_lim <- comuni_map$pop_lim + comuni_map$pop_lim2 
+
+comuni_map$perc_over65_tot_pop_lim <- (comuni_map$pop_lim/comuni_map$tot_pop_lim) 
+comuni_map$perc_over80_tot_pop_lim <- (comuni_map$pop_lim2/comuni_map$tot_pop_lim) 
+
+#Popolazione under 
+dato <- residenti_eta %>% 
+  filter(eta < lim_giovani) %>% 
+  group_by(cod_istat) %>% 
+  summarise(pop_giovani = sum(value))
+
+comuni_map <- left_join(comuni_map, dato, by = "cod_istat")
+
+#Indice invecchiamento
+comuni_map <- comuni_map %>%   #percentuale under
+  mutate(perc_giovani = pop_giovani/popolazione)
+
+comuni_map$indice_inv <- comuni_map$perc_lim/comuni_map$perc_giovani
+
 # Aggiungere altri dati per ciascun comune
 
 
