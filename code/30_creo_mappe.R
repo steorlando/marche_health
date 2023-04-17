aaaa5c36598d26347a8a9e78b3a853ea3dbdd11b
steorlando <steorlando@gmail.com>
2023-04-17 16:14:14 +0200

    piccole modifiche sulle mappe

--- code/30_creo_mappe.R
+++ code/30_creo_mappe.R
@@ -1,15 +1,18 @@
 
 # Anziani sopra il primo limite
-creo_mappa(dato = "pop_lim", titolo = paste0("Numero anziani sopra ",lim_eta," anni"))
+creo_mappa(dato = "over65", titolo = paste0("Numero anziani sopra ",lim_eta," anni"))
 
 # Anziani sopra il secondo limite
-creo_mappa(dato = "pop_lim2", titolo = paste0("Numero anziani sopra ",lim_eta2," anni"))
+creo_mappa(dato = "over80", titolo = paste0("Numero anziani sopra ",lim_eta2," anni"))
 
 # Proporzione pop sopra il primo limite
-creo_mappa(dato = "perc_lim", titolo = paste0("Proporzione anziani sopra ",lim_eta," anni su popolazione totale"))
+creo_mappa(dato = "perc_65", titolo = paste0("Proporzione anziani sopra ",lim_eta," anni su popolazione totale"))
 
 # Proporzione pop sopra il primo limite
-creo_mappa(dato = "perc_lim2", titolo = paste0("Proporzione anziani sopra ",lim_eta2," anni su popolazione totale"))
+creo_mappa(dato = "perc_80", titolo = paste0("Proporzione anziani sopra ",lim_eta2," anni su popolazione totale"))
+
+# Indice di invecchiamento
+creo_mappa(dato = "indice_inv", titolo = paste0("Indice di invecchiamento"))
 
 
 # Stesse mappe con proporzione ma solo per comuni con popolazione oltre una certa soglia
